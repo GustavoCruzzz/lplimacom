d09fac4168fba0f6605c0f061b39d78cec12b404
GustavoCruzzz <gustavocruz.silva@ucsal.edu.br>
2026-01-29 20:22:51 -0300

    First site model

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { Servicos } from './components/servicos/servicos';
+import { Clientes } from './components/clientes/clientes';
+import { Depoimentos } from './components/depoimentos/depoimentos';
+import { Footer } from './components/footer/footer';
+import { SocialMedia } from './components/social-media/social-media';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,
-    NavbarComponent
+    NavbarComponent,
+    Hero,
+    Servicos,
+    Clientes,
+    Depoimentos,
+    Footer,
+    SocialMedia
   ],
-  templateUrl: './app.html',
+  templateUrl: './app.html'
 })
 export class AppComponent {}
