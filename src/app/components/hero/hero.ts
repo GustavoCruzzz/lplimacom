460c1bf3fed6517639a4db8ffb9e6a1bdfdeb88f
GustavoCruzzz <gustavocruz.silva@ucsal.edu.br>
2026-01-29 23:21:11 -0300

    fixed hero problems

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,21 +1,30 @@
-import { Component, AfterViewInit } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importante para o [class] funcionar se não for standalone puro
+import { Component, AfterViewInit, Inject, PLATFORM_ID, ChangeDetectorRef } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-hero',
-  standalone: true, // Garanta que está como standalone se seu projeto for assim
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
 export class Hero implements AfterViewInit {
-  // Variável de controle
   showAnimation = false;
 
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    private cdr: ChangeDetectorRef // Injeção necessária para forçar a atualização
+  ) {}
+
   ngAfterViewInit() {
-    // Pequeno delay para garantir que a hidratação terminou
-    setTimeout(() => {
-      this.showAnimation = true;
-    }, 100);
+    // Verifica se estamos no navegador (para não dar erro no Server-Side)
+    if (isPlatformBrowser(this.platformId)) {
+      setTimeout(() => {
+        this.showAnimation = true;
+        
+        // O SEGREDO: Força o Angular a detectar a mudança e aplicar a classe na hora
+        this.cdr.detectChanges(); 
+      }, 100);
+    }
   }
 }
