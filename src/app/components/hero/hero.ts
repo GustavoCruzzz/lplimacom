6ffa50f69246bf77ce00f89567d7090b114a47a8
GustavoCruzzz <gustavocruz.silva@ucsal.edu.br>
2026-01-29 22:57:45 -0300

    First hero changes

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importante para o [class] funcionar se não for standalone puro
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  standalone: true, // Garanta que está como standalone se seu projeto for assim
+  imports: [CommonModule],
   templateUrl: './hero.html',
   styleUrl: './hero.css',
 })
-export class Hero {
+export class Hero implements AfterViewInit {
+  // Variável de controle
+  showAnimation = false;
 
+  ngAfterViewInit() {
+    // Pequeno delay para garantir que a hidratação terminou
+    setTimeout(() => {
+      this.showAnimation = true;
+    }, 100);
+  }
 }
